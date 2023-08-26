05d12e4a0725146c96d2bfe6b380d5c2cd60f51b
Saffi <saffi@saffi.dev>
2023-08-26 16:59:01 +0100

    chore: add www-saffi{,-dev,-wtf} crates

---
+++ www-saffi-dev/src/main.rs
@@ -0,0 +1,6 @@
+use www_saffi::add;
+
+fn main() {
+    let result = add(2, 2);
+    println!("2 + 2 = {result}");
+}
---
+++ www-saffi-wtf/src/main.rs
@@ -0,0 +1,6 @@
+use www_saffi::add;
+
+fn main() {
+    let result = add(60, 9);
+    println!("{result} nice");
+}
